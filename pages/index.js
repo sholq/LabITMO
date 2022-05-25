d417bda3b8d52f65a273384453764347f886c986
Foxylab <rushan.shakirzianov@gmail.com>
2022-05-25 09:20:50 +0300

    feat: add scroll by wheel. Clean up: remove unused markup

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var splide = new Splide(".splide", {
     type: "loop",
     padding: "1.5rem",
+    wheel: true,
   });
   splide.mount();
 });
